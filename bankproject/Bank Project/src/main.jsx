21bf98b0f6ee0d7e009d2ca3727bf7fba5ca338b
Mateus Vieira <mateuss.vieira.dev@gmail.com>
2022-12-21 21:17:41 -0300

    first commit

---
+++ bankproject/Bank Project/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Apresentation from './pages/Home/Apresentation'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Apresentation></Apresentation>
+  </React.StrictMode>,
+)
---
+++ bankproject/Bank Project/src/pages/Home/Apresentation.jsx
@@ -0,0 +1,10 @@
+
+function Apresentation() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!!!
+    </h1>
+  )
+}
+
+export default Apresentation
