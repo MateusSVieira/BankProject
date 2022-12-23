d929e2a75a762bfbbe5d37f701a1010387a4d6a0
Mateus Vieira <mateuss.vieira.dev@gmail.com>
2022-12-22 21:34:07 -0300

    Primeira tela de apresentação: Pronta.

--- bankproject/Bank Project/src/pages/Home/Apresentation.jsx
+++ bankproject/Bank Project/src/pages/Home/Apresentation.jsx
@@ -4,14 +4,14 @@ export function Apresentation() {
 
   return (
 
-    <div className="bg-orange-500 w-full h-full flex justify-center items-center">
-        <div className="flex flex-col justify-center items-center">
+    <div className=" flex justify-center items-center content-center">
+        <div className=" bg-Yale Blue w-80 h-screen md:bg-Yale-Blue lg:bg-Yale-Blue xl:bg-Yale-Blue flex flex-col justify-center items-center">
             <div>
                 <img src={apresetationImg} alt="" />
             </div>
             <h1 className="text-2xl">Finity</h1>
             <p>Money Transfer Wallet & Finance UI Kit</p>
-            <button className="border-solid border-2 border-black">Get Started Now</button>
+            <button className="w-28 h- bg-Orange-Web">Get Started Now</button>
 
         </div>
     </div>
--- bankproject/Bank Project/tailwind.config.cjs
+++ bankproject/Bank Project/tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "Orange-Web" : "#FFA400",
+        "Yale-Blue" : "#054A91",
+        "Steel-blue" : "#3E7CB1",
+        "Platinum" : "#E0E1DD"
+      },
+    },
   },
   plugins: [],
 }
